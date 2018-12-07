e06f687f8a36163ff26f3146611921fb1af3fc45
Ilya Liakh <i.liakh@sdventures.com>
2018-12-08 00:42:53 +0300

    Initial Commit

---
+++ WheatherTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WheatherTest
+//
+//  Created by admin on 08/12/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
